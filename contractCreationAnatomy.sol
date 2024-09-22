2caa41bbe975b37e30739d54203ed9f35c84651f
SABIRUDDIN, SK <SK.Sabiruddin@ellucian.com>
2024-09-22 22:14:05 +0530

    Advanced solidity

---
+++ contractCreationAnatomy.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.27;
+
+// during construction, we have our "lot" created, but no house (runtime code) yet!
+// this is so we can:
+// 1) send money to the house (aka we have a mailbox associated with our lot, but no house yet)
+// 2) access storage associated with that address (we can put furniture and packages on the lot, but still no house!)
+/**
+ * A contract does not exist on-chain before the constructor is done.
+ */
+contract PlaceHolderAddress {
+    uint x = 1;
+
+    constructor() payable {
+        require(address(this).balance == 1 ether);
+        require(msg.value == 1 ether);
+
+        require(myself() == 4);
+        require(x == 1);
+    }
+
+    function myself() internal pure returns (uint) {
+        return 4;
+    } 
+}
---
+++ modifierBestWay.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+}
+contract MyContract {
+    address private _owner;
+
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    function _checkOwner() internal view virtual {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    }
+
+    function restrictedFunction() public onlyOwner {
+        // Function logic that only the owner can execute
+    }
+}
